b4759da8ce95ed1331f0c0415ee30156295f31aa
Grzegorz <gkopacki92@gmail.com>
2024-05-19 23:25:53 +0200

    created structure for Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
 
 export function Hero() {
-	return <div></div>;
+	return (
+		<div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+			<div className="flex-1 pt-36 padding-x">
+				<h1 className="hero__title">
+					Find, book, or rent a car — quickly and easily!
+				</h1>
+				<p className="hero__subtitle">
+					Streamline your car rental experience with our effortles booking
+					process.
+				</p>
+			</div>
+		</div>
+	);
 }
