8c4b528c99e532c87b2932ffa180429d8d0a6661
Grzegorz <gkopacki92@gmail.com>
2024-05-21 12:00:07 +0200

    created navbar component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,12 +7,13 @@ export function CustomButton({
 	title,
 	containerStyles,
 	handleClick,
+	btnType,
 }: CustomButtonProps) {
 	return (
 		<button
 			className={`custom-btn ${containerStyles}`}
 			disabled={false}
-			type={"button"}
+			type={btnType || "button"}
 			onClick={handleClick}
 		>
 			<span className={`flex-1`}>{title}</span>
