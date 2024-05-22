cdf2103132ffc0f20b51b2ad640defdd5b1c9541
Grzegorz <gkopacki92@gmail.com>
2024-05-22 09:47:15 +0200

    created new variable isDataEmpty

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 export default async function Home() {
 	const allCars = await fetchCars();
+	const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 	return (
 		<main className="overflow-hidden">
 			<Hero />
@@ -19,6 +20,17 @@ export default async function Home() {
 						<CustomFilter title="year" />
 					</div>
 				</div>
+
+				{!isDataEmpty ? (
+					<section>
+						<div className="home__cars-wrapper"></div>
+					</section>
+				) : (
+					<div className="home__error-container">
+						<h2 className="text-black text-xl">Oops, no results</h2>
+						<p>{allCars?.message}</p>
+					</div>
+				)}
 			</div>
 		</main>
 	);
