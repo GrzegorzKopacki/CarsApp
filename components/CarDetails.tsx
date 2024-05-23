289b3cc4735a9db83f305e82115572f6f098b8b1
Grzegorz <gkopacki92@gmail.com>
2024-05-23 23:20:44 +0200

    import dialogpanel for carDetails component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -3,7 +3,7 @@
 import { CarProps } from "@/types";
 import Image from "next/image";
 import { Fragment } from "react";
-import { Dialog, Transition, TransitionChild } from "@headlessui/react";
+import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
 
 interface CarDetailsProps {
 	isOpen: boolean;
@@ -27,6 +27,23 @@ export function CarDetails({ isOpen, closeModal, car }: CarDetailsProps) {
 					>
 						<div className="fixed inset-0 bg-black bg-opacity-25"></div>
 					</TransitionChild>
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<TransitionChild
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+                                <DialogPanel>
+                                    <button></button>
+                                </DialogPanel>
+                            </TransitionChild>
+						</div>
+					</div>
 				</Dialog>
 			</Transition>
 		</>
