6eaed48a1e0f30dc99f60e325ed062a106f0efaa
Grzegorz <gkopacki92@gmail.com>
2024-05-23 23:10:31 +0200

    import transtionchild in carDetails component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import { CarProps } from "@/types";
+import Image from "next/image";
+import { Fragment } from "react";
+import { Dialog, Transition, TransitionChild } from "@headlessui/react";
 
 interface CarDetailsProps {
 	isOpen: boolean;
@@ -6,6 +11,24 @@ interface CarDetailsProps {
 	car: CarProps;
 }
 
-export function CarDetails() {
-	return <div></div>;
+export function CarDetails({ isOpen, closeModal, car }: CarDetailsProps) {
+	return (
+		<>
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={closeModal}>
+					<TransitionChild
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black bg-opacity-25"></div>
+					</TransitionChild>
+				</Dialog>
+			</Transition>
+		</>
+	);
 }
