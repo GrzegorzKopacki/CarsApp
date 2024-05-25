67600227a5fd910c931a5b668c67ba1165a81f50
Grzegorz <gkopacki92@gmail.com>
2024-05-25 22:28:08 +0200

    added function generateCarImageUrl in carDetails and carCard component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -9,6 +9,7 @@ import {
 	Transition,
 	TransitionChild,
 } from "@headlessui/react";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDetailsProps {
 	isOpen: boolean;
@@ -60,7 +61,7 @@ export function CarDetails({ isOpen, closeModal, car }: CarDetailsProps) {
 									<div className="flex-1 flex flex-col gap-3">
 										<div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
 											<Image
-												src="/hero.png"
+												src={generateCarImageUrl(car, "angle")}
 												alt="car model"
 												fill
 												priority
@@ -70,7 +71,7 @@ export function CarDetails({ isOpen, closeModal, car }: CarDetailsProps) {
 										<div className="flex gap-3">
 											<div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
 												<Image
-													src="/hero.png"
+													src={generateCarImageUrl(car, "angle")}
 													alt="car model"
 													fill
 													priority
@@ -79,7 +80,7 @@ export function CarDetails({ isOpen, closeModal, car }: CarDetailsProps) {
 											</div>
 											<div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
 												<Image
-													src="/hero.png"
+													src={generateCarImageUrl(car, "angle")}
 													alt="car model"
 													fill
 													priority
@@ -88,7 +89,7 @@ export function CarDetails({ isOpen, closeModal, car }: CarDetailsProps) {
 											</div>
 											<div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
 												<Image
-													src="/hero.png"
+													src={generateCarImageUrl(car, "angle")}
 													alt="car model"
 													fill
 													priority
